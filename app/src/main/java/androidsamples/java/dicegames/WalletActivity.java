6525d2ecdc5e957300d019c41ca529ffa21b95b6
realdev12 <tejasadsovani@gmail.com>
2024-09-06 17:40:36 +0530

    Using View Model

--- app/src/main/java/androidsamples/java/dicegames/WalletActivity.java
+++ app/src/main/java/androidsamples/java/dicegames/WalletActivity.java
@@ -7,26 +7,12 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 public class WalletActivity extends AppCompatActivity {
-    private int mBalance;
-    private int mSixes;
-    private int mRolls;
-    private int mDoubleSixes;
-    private int mDoubleOthers;
-    private int mPrev;
-    private int mDieValue;
-    private Die mDie;
-
-    private static final String KEY_BALANCE = "KEY_BALANCE";
-    private static final String KEY_SIXES = "KEY_SIXES";
-    private static final String KEY_ROLLS = "KEY_ROLLS";
-    private static final String KEY_DOUBLESIXES = "KEY_DOUBLESIXES";
-    private static final String KEY_DOUBLEOTHERS = "KEY_DOUBLEOTHERS";
-    private static final String KEY_PREV = "KEY_PREV";
-    private static final String KEY_VALUE = "KEY_VALUE";
     private TextView mTxtBalance;
     private Button mBtnDie;
     private TextView mTxtSingleSixes;
@@ -34,9 +20,8 @@ public class WalletActivity extends AppCompatActivity {
     private TextView mTxtDoubleSixes;
     private TextView mTxtDoubleOthers;
     private TextView mTxtPrevRoll;
+    private WalletViewModel mWalletVM;
     private static final String TAG="WalletActivity";
-    private static final int WIN_VALUE = 6;
-    private static final int INCREMENT = 5;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +29,6 @@ public class WalletActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_wallet);
 
-    mDie = new Die6();
     mTxtBalance = findViewById(R.id.txt_balance);
     mBtnDie = findViewById(R.id.btn_die);
     mTxtSingleSixes = findViewById(R.id.txt_single_sixes);
@@ -53,74 +37,62 @@ public class WalletActivity extends AppCompatActivity {
     mTxtDoubleOthers = findViewById(R.id.txt_double_others);
     mTxtPrevRoll = findViewById(R.id.txt_prev_roll);
 
-      if(savedInstanceState != null){
-          mBalance = savedInstanceState.getInt(KEY_BALANCE, 0);
-          mSixes = savedInstanceState.getInt(KEY_SIXES, 0);
-          mPrev = savedInstanceState.getInt(KEY_PREV, 0);
-          mDoubleSixes = savedInstanceState.getInt(KEY_DOUBLESIXES, 0);
-          mDoubleOthers = savedInstanceState.getInt(KEY_DOUBLEOTHERS, 0);
-          mRolls = savedInstanceState.getInt(KEY_ROLLS, 0);
-          mDieValue = savedInstanceState.getInt(KEY_VALUE, 0);
+    mWalletVM = new ViewModelProvider(this).get(WalletViewModel.class);
+    updateUI();
 
-          updateUI();
-      }
+//      if(savedInstanceState != null){
+//          mBalance = savedInstanceState.getInt(KEY_BALANCE, 0);
+//          mSixes = savedInstanceState.getInt(KEY_SIXES, 0);
+//          mPrev = savedInstanceState.getInt(KEY_PREV, 0);
+//          mDoubleSixes = savedInstanceState.getInt(KEY_DOUBLESIXES, 0);
+//          mDoubleOthers = savedInstanceState.getInt(KEY_DOUBLEOTHERS, 0);
+//          mRolls = savedInstanceState.getInt(KEY_ROLLS, 0);
+//          mDieValue = savedInstanceState.getInt(KEY_VALUE, 0);
+//
+//          updateUI();
+//      }
 
     mBtnDie.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        mDie.roll();
-        mDieValue = mDie.value();
-        mRolls++; Log.d(TAG, "Die roll = " + mDieValue + " Rolls: " + mRolls);
-        if(mDieValue == WIN_VALUE){
-          mBalance += INCREMENT;
-          if(mPrev == WIN_VALUE) {
-              mBalance += INCREMENT;
-              mDoubleSixes++;
-          }
-          mSixes++; Log.d(TAG, "New Sixes = " + mSixes);
-          showToast();
-        }
-        else if(mPrev == mDieValue) {
-            mBalance -= INCREMENT;
-            mDoubleOthers++;
-        }
-        Log.d(TAG, "New Balance = " + mBalance);
-        mPrev = mDieValue;
+        mWalletVM.rollDie();
         updateUI();
+        if(mWalletVM.dieValue() == mWalletVM.WIN_VALUE){
+            showToast("Congratulations!");
+        }
       }
     });
   }
 
   private void updateUI(){
-    mTxtBalance.setText(Integer.toString(mBalance));
-    mBtnDie.setText(Integer.toString(mDieValue));
-    mTxtSingleSixes.setText(Integer.toString(mSixes));
-    mTxtTotalRolls.setText(Integer.toString(mRolls));
-    mTxtDoubleOthers.setText(Integer.toString(mDoubleOthers));
-    mTxtDoubleSixes.setText(Integer.toString(mDoubleSixes));
-    mTxtPrevRoll.setText(Integer.toString(mPrev));
+    mTxtBalance.setText(Integer.toString(mWalletVM.balance()));
+    mBtnDie.setText(Integer.toString(mWalletVM.dieValue()));
+    mTxtSingleSixes.setText(Integer.toString(mWalletVM.singleSixes()));
+    mTxtTotalRolls.setText(Integer.toString(mWalletVM.totalRolls()));
+    mTxtDoubleOthers.setText(Integer.toString(mWalletVM.doubleOthers()));
+    mTxtDoubleSixes.setText(Integer.toString(mWalletVM.doubleSixes()));
+    mTxtPrevRoll.setText(Integer.toString(mWalletVM.previousRoll()));
 
   }
 
-  private void showToast(){
-      CharSequence text = "Congratulations!";
+  private void showToast(CharSequence text){
       int duration = Toast.LENGTH_SHORT;
       Toast toast = Toast.makeText(this, text, duration);
       toast.show();
   }
 
   @Override
-  protected void onSaveInstanceState(@NonNull Bundle outState){
-    super.onSaveInstanceState(outState);
-    Log.d(TAG, "outSaveInstanceState");
-    outState.putInt(KEY_BALANCE, mBalance);
-    outState.putInt(KEY_PREV, mPrev);
-    outState.putInt(KEY_DOUBLEOTHERS, mDoubleOthers);
-    outState.putInt(KEY_DOUBLESIXES, mDoubleSixes);
-    outState.putInt(KEY_ROLLS, mRolls);
-    outState.putInt(KEY_VALUE, mDieValue);
-    outState.putInt(KEY_SIXES, mSixes);
-  }
+//  protected void onSaveInstanceState(@NonNull Bundle outState){
+//    super.onSaveInstanceState(outState);
+//    Log.d(TAG, "outSaveInstanceState");
+//    outState.putInt(KEY_BALANCE, mBalance);
+//    outState.putInt(KEY_PREV, mPrev);
+//    outState.putInt(KEY_DOUBLEOTHERS, mDoubleOthers);
+//    outState.putInt(KEY_DOUBLESIXES, mDoubleSixes);
+//    outState.putInt(KEY_ROLLS, mRolls);
+//    outState.putInt(KEY_VALUE, mDieValue);
+//    outState.putInt(KEY_SIXES, mSixes);
+//  }
 
   protected void onStart(){
       super.onStart();
--- app/src/main/java/androidsamples/java/dicegames/WalletViewModel.java
+++ app/src/main/java/androidsamples/java/dicegames/WalletViewModel.java
@@ -1,14 +1,40 @@
 package androidsamples.java.dicegames;
 
+import android.util.Log;
+
 import androidx.lifecycle.ViewModel;
 
 public class WalletViewModel extends ViewModel {
+  private static final String TAG="WalletViewModel";
+  public static final int WIN_VALUE = 6;
+  public static final int INCREMENT = 5;
 
+  private int mBalance;
+  private int mSixes;
+  private int mRolls;
+  private int mDoubleSixes;
+  private int mDoubleOthers;
+  private int mPrev;
+  private Die mDie;
+
+//  private static final String KEY_BALANCE = "KEY_BALANCE";
+//  private static final String KEY_SIXES = "KEY_SIXES";
+//  private static final String KEY_ROLLS = "KEY_ROLLS";
+//  private static final String KEY_DOUBLESIXES = "KEY_DOUBLESIXES";
+//  private static final String KEY_DOUBLEOTHERS = "KEY_DOUBLEOTHERS";
+//  private static final String KEY_PREV = "KEY_PREV";
   /**
    * The no argument constructor.
    */
   public WalletViewModel() {
     // TODO implement method
+    mBalance = 0;
+    mSixes = 0;
+    mRolls = 0;
+    mDoubleSixes = 0;
+    mDoubleOthers = 0;
+    mPrev = 0;
+    mDie = new Die6();
   }
 
   /**
@@ -17,7 +43,7 @@ public class WalletViewModel extends ViewModel {
    */
   public int balance() {
     // TODO implement method
-    return 0;
+    return mBalance;
   }
 
   /**
@@ -25,15 +51,30 @@ public class WalletViewModel extends ViewModel {
    */
   public void rollDie() {
     // TODO implement method
+    mDie.roll();
+    mRolls++; Log.d(TAG, "Die roll = " + mDie.value() + " Rolls: " + mRolls);
+    if(mDie.value() == WIN_VALUE){
+      mBalance += INCREMENT;
+      if(mPrev == WIN_VALUE) {
+        mBalance += INCREMENT;
+        mDoubleSixes++;
+      }
+      mSixes++; Log.d(TAG, "New Sixes = " + mSixes);
+    }
+    else if(mPrev == mDie.value()) {
+      mBalance -= INCREMENT;
+      mDoubleOthers++;
+    }
+    Log.d(TAG, "New Balance = " + mBalance);
+    mPrev = mDie.value();
   }
-
   /**
    * Reports the current value of the {@link Die}.
    *
    */
   public int dieValue() {
     // TODO implement method
-    return 0;
+    return mDie.value();
   }
 
   /**
@@ -42,7 +83,7 @@ public class WalletViewModel extends ViewModel {
    */
   public int singleSixes() {
     // TODO implement method
-    return 0;
+    return mSixes;
   }
 
   /**
@@ -51,7 +92,7 @@ public class WalletViewModel extends ViewModel {
    */
   public int totalRolls() {
     // TODO implement method
-    return 0;
+    return mRolls;
   }
 
   /**
@@ -60,7 +101,7 @@ public class WalletViewModel extends ViewModel {
    */
   public int doubleSixes() {
     // TODO implement method
-    return 0;
+    return mDoubleSixes;
   }
 
   /**
@@ -69,7 +110,7 @@ public class WalletViewModel extends ViewModel {
    */
   public int doubleOthers() {
     // TODO implement method
-    return 0;
+    return mDoubleOthers;
   }
 
   /**
@@ -78,6 +119,6 @@ public class WalletViewModel extends ViewModel {
    */
   public int previousRoll() {
     // TODO implement method
-    return 0;
+    return mPrev;
   }
 }
